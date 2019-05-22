2c790d86283ee0cb249b0b220a9a269e79592c2b
BillXu <xuwenyanbill@gmail.com>
2019-05-22 20:59:32 +0800

    before modify dlgBase

---
+++ Assets/Script/Common/ToggleToSwitch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Toggle))]
+public class ToggleToSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Image mBackGroundImg = null ;
+    void Start()
+    {
+        var tog = this.transform.GetComponent<Toggle>();
+        if ( tog == null )
+        {
+            Debug.LogError("must use with toggle control");
+        }
+
+        var bgNode = this.transform.GetChild( 0 );
+        if ( bgNode == null || bgNode.gameObject.name != "Background" )
+        {
+            Debug.LogError("invalid toggle");
+            return ;
+        }
+
+        this.mBackGroundImg = bgNode.GetComponent<Image>();
+        if ( null == this.mBackGroundImg )
+        {
+            Debug.LogError("back ground do not have image component");
+            return ;
+        }
+
+        if ( tog.onValueChanged == null )
+        {
+            tog.onValueChanged = new Toggle.ToggleEvent();
+        }
+        tog.onValueChanged.AddListener(this.onClickSwitch) ;
+        this.onClickSwitch(tog.isOn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onClickSwitch( bool isCheck )
+    {
+        this.mBackGroundImg.enabled = !isCheck ;
+    }
+}
---
+++ Assets/Script/Scene/RoomScene/LayerDlg.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerDlg : MonoBehaviour
+{
+    public void onClickLoaction()
+    {
+
+    }
+
+    public void onClickCopyRoomNum()
+    {
+
+    }
+
+    public void onClickInviteFriends()
+    {
+
+    }
+
+    public void onClickShowMore( bool isShow )
+    {
+
+    }
+
+}
